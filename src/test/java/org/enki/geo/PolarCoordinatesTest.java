d081a1964827dd8d4f2cc334e79c2ab995364790
Gene McCulley <mcculley@stackframe.com>
2023-03-16 15:41:55 -0400

    Add unit test.

---
+++ src/test/java/org/enki/geo/PolarCoordinatesTest.java
@@ -0,0 +1,21 @@
+package org.enki.geo;
+
+import org.junit.jupiter.api.Test;
+import tech.units.indriya.quantity.Quantities;
+
+import java.awt.geom.Point2D;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static systems.uom.common.USCustomary.DEGREE_ANGLE;
+
+public class PolarCoordinatesTest {
+
+    @Test
+    public void testSimple() {
+        final PolarCoordinates c = new PolarCoordinates(1.0, Quantities.getQuantity(0, DEGREE_ANGLE));
+        final Point2D.Double p = c.toCartesian();
+        assertEquals(1, p.x);
+        assertEquals(0, p.y);
+    }
+
+}
