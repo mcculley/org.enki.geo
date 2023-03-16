c099233d0930c5c90e877f8518d0bc3890372ad6
Gene McCulley <mcculley@stackframe.com>
2023-03-16 09:24:39 -0400

    Add more unit tests.

--- src/test/java/org/enki/geo/LatLongElevationTest.java
+++ src/test/java/org/enki/geo/LatLongElevationTest.java
@@ -3,16 +3,31 @@ package org.enki.geo;
 import org.junit.jupiter.api.Test;
 import tech.units.indriya.quantity.Quantities;
 
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static tech.units.indriya.unit.Units.METRE;
 
 public class LatLongElevationTest {
 
     @Test
-    public void testToString() {
+    public void testDistance() {
         final LatLongElevation l1 = new LatLongElevation(50, 20, Quantities.getQuantity(0, METRE));
-        final LatLongElevation l2 = new LatLongElevation(new LatLong(50,20), Quantities.getQuantity(100, METRE));
+        final LatLongElevation l2 = new LatLongElevation(new LatLong(50, 20), Quantities.getQuantity(100, METRE));
         assertEquals(100, l1.distance(l2).getValue().doubleValue());
+    }
+
+    @Test
+    public void testEquality() {
+        final LatLongElevation l1 = new LatLongElevation(50, 20, Quantities.getQuantity(0, METRE));
+        final LatLongElevation l2 = new LatLongElevation(new LatLong(50, 20), Quantities.getQuantity(0, METRE));
+        assertEquals(l1, l2);
+    }
+
+    @Test
+    public void testHashCode() {
+        final LatLongElevation l1 = new LatLongElevation(50, 20, Quantities.getQuantity(0, METRE));
+        assertEquals(9, Set.of(l1, "2", "3", "4", "5", "6", "7", "8", "9").size());
     }
 
 }
